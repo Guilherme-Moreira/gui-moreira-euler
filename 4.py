4836fc63512ad3a92f7b4ed1ee3852f34b352aeb
Guilherme-Moreira <guimoreiraheroes@gmail.com>
2019-03-08 22:06:01 -0300

    fixed

--- 4.py
+++
@@ -1,8 +0,0 @@
-answ = 0
-
-for a in range(100, 1000):
-    for b in range(a, 1000):
-        if str(a*b) == str(a*b)[::-1] and a*b > answ:
-            answ = a*b
-
-print(answ)
--- 6.py
+++
@@ -1,8 +0,0 @@
-a, b = 0, 0
-
-for i in range(1, 101):
-    a += i
-    b = b + i ** 2
-
-a = a ** 2
-print(a - b)  # 25164150
