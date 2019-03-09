b3aa00419d3f2894ae93de5da6f9b22ab4931330
Guilherme-Moreira <guimoreiraheroes@gmail.com>
2019-03-08 21:58:45 -0300

    7 and 8

--- 6.py
+++ 6.py
@@ -1,23 +1,8 @@
-import math
+a, b = 0, 0
 
+for i in range(1, 101):
+    a += i
+    b = b + i ** 2
 
-def isPrime(n):
-    prime = True
-    if n >= 2:
-        for i in range(2, int(math.ceil(math.sqrt(n))) + 1):
-            if n % i == 0:
-                prime = False
-    elif n != 1:
-        prime = True
-    return prime
-
-
-nPrimes = 0
-i = 0
-while nPrimes <= 10001:
-    if isPrime(i):
-        nPrimes += 1
-
-    i += 1
-
-print(i-1)  # 104743
+a = a ** 2
+print(a - b)  # 25164150
